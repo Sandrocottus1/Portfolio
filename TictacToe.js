6953be9f13778f6c9ad4859255cb80883203928c
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2026-02-09 02:17:58 +0530

    major changes in portfolio layout and addition of new projects

---
+++ TictacToe.js
@@ -0,0 +1,30 @@
+let board=[
+    ['','','O'],
+    ['','X',''],
+    ['','',''],
+];
+let player1='X';
+let player2='O';
+function setup(){
+    createCanvas(400,400);
+}
+function draw(){
+    background(220);
+    let w=width/3;
+    let h=height/3;
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            let x=w*i;
+            let y=h*j;
+            let spot=board[i][j];
+            textSize(32);
+            if(spot==player1){
+                ellipse(x,y,w);
+            }else if (spot==player2){
+                line(x,y,x+w,y+h);
+                line(x+w,y,x,y+h);
+            }
+            
+        }
+    }
+}
